db37881446f40be3a92425fd0d5f55d617ed52f3
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-12-19 00:37:39 +0530

    19-12-22

---
+++ JS-C1/01-Fundamentals-Part-1/starter - Copy/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+
+// if (js === "amazing")
+// {
+//     alert("js is Fun..!");
+// }
+
+console.log(100 - 20 + 1);
+
+//  variable is to store the values, this in format of camelCase, Using _ (or) $
+let firstName = "Uday_Kiran";
+let last_name = "Sugali"; 
+let _firstJob = "Ansible_DEV";
+let PI = 3.41;                       // Constant variables should be cap..
+let $presentJob = "SDE";
+
+let job1 = "Ansible_DEV";
+let job2 = "SDE";   // variable can't be predefined name like name, fun, cap letter.. 
+
+console.log(firstName);
+console.log(last_name);
+console.log($presentJob, _firstJob);
+console.log(js);
