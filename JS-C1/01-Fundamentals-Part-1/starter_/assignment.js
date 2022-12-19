173f3a5e222c382fc9c86369a58b7bc7b86efa07
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-12-19 23:39:02 +0530

    19-12-22

---
+++ JS-C1/01-Fundamentals-Part-1/starter_/assignment.js
@@ -0,0 +1,65 @@
+// part -1
+//  var, values 
+let continent = 'Asia';
+let country = 'India';
+let population = 13;
+
+// Data types
+let isIsland = false;
+let language;
+
+console.log(continent);
+console.log(country);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
+
+// let, const, var
+language = 'telugu'; 
+const love = 134;
+var capitalState = 'andhraPradesh';
+
+console.log(language);
+console.log(love);
+console.log(capitalState);
+
+capitalState = 2;
+console.log(capitalState);
+
+
+// Basic Operators
+let halfPopulation = (population/2);
+population += 1;
+
+console.log(halfPopulation);
+console.log(population);
+
+let finland = 6; // In million
+
+// if (finland < population)
+// {
+//     console.log("my country is more people than finland");
+// }
+// else {
+//     console.log("finland is more people than my country.");
+// }
+
+let avgPopulation = 33; // In million
+
+// if (population < avgPopulation)
+// {
+//     console.log("avgPopulation is more people than my country.");
+// }
+// else {
+//     console.log("my country is more people than avgPopulation");
+// }
+
+console.log(finland < population);
+console.log(population > avgPopulation);
+
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+
+//  strings and Template Literals
+
